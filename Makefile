753d2145deada85e1a12c0dadc56a5e6da81508a
Ben Zhang <ben.zhang@watonomous.ca>
2023-07-30 22:04:58 +0000

    0.2.3

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ HOSTNAME=watonomous.ca
 NAMESPACE=tf
 NAME=email
 BINARY=terraform-provider-${NAME}
-VERSION=0.2.1
+VERSION=0.2.3
 OS_ARCH=linux_amd64
 
 default: install
--- examples/main.tf
+++ examples/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     email = {
-      version = "0.2.1"
+      version = "0.2.3"
       source  = "watonomous.ca/tf/email"
     }
   }
